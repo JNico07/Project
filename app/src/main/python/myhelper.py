6951999dbec48789298248189903134b9577e83d
Nico <nicoolano43@gmail.com>
2024-02-03 20:13:04 +0800

    Test

---
+++ app/src/main/python/myhelper.py
@@ -0,0 +1,9 @@
+
+def main(number1, number2):
+    num1 = int(number1)
+    num2 = int(number2)
+
+    summ = num1 + num2
+
+    print("Sum is:", summ)  # Add this line to print the result to the console
+    return "sum is " + str(summ)
